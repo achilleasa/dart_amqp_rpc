caa49ee7d0ab5ec9006b00701d55909a979a34cd
Achilleas Anagnostopoulos <achilleas@taxibeat.com>
2015-05-03 22:29:43 +0300

    Added missing docblocks

--- examples/fib_msgpack/lib/src/msgpack_codec.dart
+++ examples/fib_msgpack/lib/src/msgpack_codec.dart
@@ -3,12 +3,19 @@ part of dart_ampq_rpc.examples.msgpack;
 // The protobuf codec with custom error handling
 class MsgpackCodec extends MsgpackRpcCodec {
 
+  /**
+   * Encode a caught [error] into a [Uint8List]
+   */
   Future<Uint8List> encodeError(Object error) {
     RpcError errorMessage = new RpcError(error.toString());
 
     return new Future.value(new Uint8List.fromList(packer.packMessage(errorMessage)));
   }
 
+  /**
+   * Decode a [response] containing a caught error message and
+   * return back a failed [Future] with the error message
+   */
   Future<Object> decodeError(Uint8List response) {
     return new Future.error(
         unpacker(response).unpackMessage(RpcError.fromList)
--- examples/fib_protobuf/lib/src/protobuf_codec.dart
+++ examples/fib_protobuf/lib/src/protobuf_codec.dart
@@ -3,6 +3,9 @@ part of dart_ampq_rpc.examples.protobuf;
 // The protobuf codec with custom error handling
 class ProtobufCodec extends ProtobufRpcCodec {
 
+  /**
+   * Encode a caught [error] into a [Uint8List]
+   */
   Future<Uint8List> encodeError(Object error) {
     RpcError errorMessage = new RpcError()
       ..message = error.toString();
@@ -10,6 +13,10 @@ class ProtobufCodec extends ProtobufRpcCodec {
     return new Future.value(errorMessage.writeToBuffer());
   }
 
+  /**
+   * Decode a [response] containing a caught error message and
+   * return back a failed [Future] with the error message
+   */
   Future<Object> decodeError(Uint8List response) {
     return new Future.error(
         new RpcError.fromBuffer(response)
